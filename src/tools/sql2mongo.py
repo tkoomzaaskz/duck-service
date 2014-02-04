99f12743d3dbf84278b95f30dbbacc8692c7a99f
Tomasz Ducin <tomasz.ducin@gmail.com>
2014-02-05 00:00:50 +0100

    improving sql > mongo migrator

--- src/tools/sql2mongo.py
+++ src/tools/sql2mongo.py
@@ -16,32 +16,10 @@ class Migrator(object):
         self.mongodb = MongoClient().duck_finances
 
 class Sql2Mongo(Migrator):
-    def migrate_outcomes(self, users):
-        cursor = self.sqldb.cursor()
-        cursor.execute('SELECT id, category_id, amount, description, created_at, created_by FROM outcome')
-        return { int(row[0]): self.mongodb.outcomes.insert({
-            'category_id': int(row[1]),
-            'amount': float(row[2]),
-            'description': row[3],
-            'created_at': row[4],
-            'created_by': users[int(row[5])]
-        }) for row in cursor }
-
-    def migrate_incomes(self, users):
-        cursor = self.sqldb.cursor()
-        cursor.execute('SELECT id, category_id, amount, description, created_at, created_by FROM income')
-        return { int(row[0]): self.mongodb.incomes.insert({
-            'category_id': int(row[1]),
-            'amount': float(row[2]),
-            'description': row[3],
-            'created_at': row[4],
-            'created_by': users[int(row[5])]
-        }) for row in cursor }
-
     def migrate_users(self):
         cursor = self.sqldb.cursor()
         cursor.execute('SELECT id, first_name, last_name, email_address, username, password, last_login, created_at FROM sf_guard_user')
-        return { int(row[0]): self.mongodb.users.insert({
+        self.users = { int(row[0]): self.mongodb.users.insert({
             'first_name': row[1],
             'last_name': row[2],
             'email': row[3],
@@ -51,10 +29,50 @@ class Sql2Mongo(Migrator):
             'created_at': row[7]
         }) for row in cursor }
 
+    def migrate_outcomes(self):
+        if not hasattr(self, 'users'):
+            raise RuntimeError('users must be migrated before outcomes can be')
+        cursor = self.sqldb.cursor()
+        cursor.execute('SELECT id, category_id, amount, description, created_at, created_by FROM outcome')
+        self.outcomes = { int(row[0]): self.mongodb.outcomes.insert({
+            'category_id': int(row[1]),
+            'amount': float(row[2]),
+            'description': row[3],
+            'created_at': row[4],
+            'created_by': self.users[int(row[5])]
+        }) for row in cursor }
+
+    def migrate_incomes(self):
+        if not hasattr(self, 'users'):
+            raise RuntimeError('users must be migrated before outcomes can be')
+        cursor = self.sqldb.cursor()
+        cursor.execute('SELECT id, category_id, amount, description, created_at, created_by FROM income')
+        self.incomes = { int(row[0]): self.mongodb.incomes.insert({
+            'category_id': int(row[1]),
+            'amount': float(row[2]),
+            'description': row[3],
+            'created_at': row[4],
+            'created_by': self.users[int(row[5])]
+        }) for row in cursor }
+
+    def migrate_categories(self):
+        cursor = self.sqldb.cursor()
+        cursor.execute('SELECT id, parent_id, name, type, created_at, created_by FROM category')
+        self.categories = {}
+        for row in cursor:
+            self.categories[int(row[0])] = self.mongodb.categories.insert({
+                'parent_id': self.categories[row[1]] if row[1] else None,
+                'name': row[2],
+                'type': row[3],
+                'created_at': row[4],
+                'created_by': self.users[int(row[5])]
+            })
+
     def migrate_all(self):
-        users = self.migrate_users()
-        self.migrate_outcomes(users)
-        self.migrate_incomes(users)
+        self.migrate_users()
+        self.migrate_categories()
+        self.migrate_outcomes()
+        self.migrate_incomes()
 
 if __name__ == '__main__':
     m = Sql2Mongo(FINANCES_MYSQL)
